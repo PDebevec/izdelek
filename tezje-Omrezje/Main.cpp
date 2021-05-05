110829c2d140a305fbae28589b9b94bde1d56a57
Peter Debevec <58183800+PDebevec@users.noreply.github.com>
2021-05-05 21:28:53 +0200

    Add files via upload

--- tezje-Omrezje/Main.cpp
+++ tezje-Omrezje/Main.cpp
@@ -1,26 +1,9 @@
 #include "Omrezje.h"
 
 int main() {
-	Omrezje n({ 2,2,1 });
-	for (int i = 0; i < 10000; i++) {
-		switch (rand()%4)
-		{
-		case 0:
-			n.treniraj({ 1.f,1.f }, { 0.f });
-			break;
-		case 1:
-			n.treniraj({ 1.f,0.f }, { 1.f });
-			break;
-		case 2:
-			n.treniraj({ 0.f,1.f }, { 1.f });
-			break;
-		case 3:
-			n.treniraj({ 0.f,0.f }, { 0.f });
-			break;
-		}
-	}
-	n.izpis({ 1.f,1.f });
-	n.izpis({ 0.f,0.f });
-	n.izpis({ 0.f,1.f });
-	n.izpis({ 1.f,0.f });
+	srand((unsigned int)time(NULL));
+	Omrezje no({ 2, 2, 1 });
+
+	no.racNaprej({ 1.f, 1.f });
+	no.izpis();
 }
--- tezje-Omrezje/Matrika.h
+++ tezje-Omrezje/Matrika.h
@@ -9,29 +9,38 @@ using namespace std;
 template<typename T>
 class Matrika {
 public:
-	Matrika() {};
+	Matrika() :V(0.f), S(0.f) {};
 	Matrika(size_t v, size_t s)
 		:V(v), S(s)
 	{
-		srand((unsigned int)time(NULL));
 		vector<T> temp;
 		temp.resize(s, 0);
-		mat.resize(v, temp);
+		data.resize(v, temp);
+	}
+	void naredi(size_t v, size_t s)
+	{
+		V = v;
+		S = s;
+		vector<T> temp;
+		temp.resize(s, 0);
+		data.resize(v, temp);
 	}
 
-	//vector<vector<T>>& operator()() { return data; }
-	vector<T>& operator()(size_t i) { return mat[i]; }
-	T& operator()(size_t i, size_t j) { return mat[i][j]; }
+	vector<T>& operator()(size_t i) { return data[i]; }
+	T& operator()(size_t i, size_t j) { return data[i][j]; }
+
+	bool operator==(Matrika<T>& druga) { return (S == druga.St() && V == druga.Vr()) }
+	bool operator!=(Matrika<T>& druga) { return (S != druga.St() && V != druga.Vr()) }
 
 	Matrika<T>& operator=(Matrika<T> druga) {
 		S = druga.St();
 		V = druga.Vr();
 		vector<T> temp;
 		temp.resize(druga.St());
-		mat.resize(druga.Vr(), temp);
+		data.resize(druga.Vr(), temp);
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				mat[i][j] = druga(i, j);
+				data[i][j] = druga(i, j);
 			}
 		}
 		return *this;
@@ -39,16 +48,16 @@ public:
 	Matrika<T>& operator=(vector<vector<T>> tab) {
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				mat[i][j] = tab[i][j];
+				data[i][j] = tab[i][j];
 			}
 		}
 		return *this;
 	}
 	Matrika<T>& operator=(vector<T> tab) {
-		int k = 0;
+		size_t k = 0;
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				mat[i][j] = tab[k];
+				data[i][j] = tab[k];
 				k++;
 			}
 		}
@@ -57,17 +66,17 @@ public:
 	Matrika<T>& operator=(T t) {
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				mat[i][j] = t;
+				data[i][j] = t;
 			}
 		}
 		return *this;
 	}
 
-	Matrika<T> operator+(Matrika<T> druga) {
+	Matrika<T> operator+(Matrika<T>& druga) {
 		Matrika temp(V, S);
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				temp(i, j) = mat[i][j] + druga(i, j);
+				temp(i, j) = data[i][j] + druga(i, j);
 			}
 		}
 		return temp;
@@ -76,16 +85,16 @@ public:
 		Matrika temp(V, S);
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				temp(i, j) = mat[i][j] + t;
+				temp(i, j) = data[i][j] + t;
 			}
 		}
 		return temp;
 	}
-	Matrika<T> operator-(Matrika<T> druga) {
+	Matrika<T> operator-(Matrika<T>& druga) {
 		Matrika temp(V, S);
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				temp(i, j) = mat[i][j] - druga(i, j);
+				temp(i, j) = data[i][j] - druga(i, j);
 			}
 		}
 		return temp;
@@ -94,17 +103,18 @@ public:
 		Matrika temp(V, S);
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				temp(i, j) = mat[i][j] - t;
+				temp(i, j) = data[i][j] - t;
 			}
 		}
 		return temp;
 	}
-	Matrika<T> operator*(Matrika<T> druga) {
+	Matrika<T> operator*(Matrika<T>& druga) {
 		Matrika temp(V, druga.St());
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < druga.St(); j++) {
+				temp(i, j) = 0.f;
 				for (size_t k = 0; k < S; k++) {
-					temp(i, j) += mat[i][k] * druga(k, j);
+					temp(i, j) += data[i][k] * druga(k, j);
 				}
 			}
 		}
@@ -114,17 +124,17 @@ public:
 		Matrika temp(V, S);
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				temp(i, j) += mat[i][j] * t;
+				temp(i, j) += data[i][j] * t;
 			}
 		}
 		return temp;
 	}
-	Matrika<T> operator/(Matrika<T> druga) {
+	/*Matrika<T> operator/(Matrika<T>& druga) {
 		Matrika temp(V, druga.St());
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < druga.St(); j++) {
 				for (size_t k = 0; k < S; k++) {
-					temp(i, j) += mat[i][k] * pow(druga(k, j), -1);
+					temp(i, j) += data[i][k] * pow(druga(k, j), -1);
 				}
 			}
 		}
@@ -134,16 +144,17 @@ public:
 		Matrika temp(V, S);
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				temp(i, j) += mat[i][j] * pow(t, -1);
+				temp(i, j) += data[i][j] * pow(t, -1);
 			}
 		}
 		return temp;
 	}
+	*/
 
 	Matrika<T>& operator+=(Matrika<T> druga) {
 		for (size_t i = 0; i < druga.Vr(); i++) {
 			for (size_t j = 0; j < druga.St(); j++) {
-				mat[i][j] += druga(i, j);
+				data[i][j] += druga(i, j);
 			}
 		}
 		return *this;
@@ -151,7 +162,7 @@ public:
 	Matrika<T>& operator+=(T t) {
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				mat[i][j] += t;
+				data[i][j] += t;
 			}
 		}
 		return *this;
@@ -159,7 +170,7 @@ public:
 	Matrika<T>& operator-=(Matrika<T> druga) {
 		for (size_t i = 0; i < druga.Vr(); i++) {
 			for (size_t j = 0; j < druga.St(); j++) {
-				mat[i][j] -= druga(i, j);
+				data[i][j] -= druga(i, j);
 			}
 		}
 		return *this;
@@ -167,7 +178,7 @@ public:
 	Matrika<T>& operator-=(T t) {
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				mat[i][j] -= t;
+				data[i][j] -= t;
 			}
 		}
 		return *this;
@@ -182,7 +193,7 @@ public:
 		(*this) = (*this) * t;
 		return *this;
 	}
-	Matrika<T>& operator/=(Matrika<T> druga) {
+	/*Matrika<T>& operator/=(Matrika<T>& druga) {
 		//Matrika<T> temp = (*this) / druga;
 		(*this) = (*this) / druga;
 		return *this;
@@ -192,25 +203,26 @@ public:
 		(*this) = (*this) / t;
 		return *this;
 	}
+	*/
 
 	Matrika<T> Prenesi() {
 		Matrika<T> vrnitev(S, V);
 		for (size_t i = 0; i < S; i++) {
 			for (size_t j = 0; j < V; j++) {
-				vrnitev(i, j) = mat[j][i];
+				vrnitev(i, j) = data[j][i];
 			}
 		}
 		return vrnitev;
 	}
 
-	size_t St() { return S; }
-	size_t Vr() { return V; }
-	size_t dolzina() { return V * S; }
+	size_t St() const { return S; }
+	size_t Vr() const { return V; }
+	size_t dolzina() const { return V * S; }
 
 	void izpis() {
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				cout << mat[i][j] << ",\t";
+				cout << data[i][j] << ",\t";
 			}
 			cout << endl;
 		}
@@ -218,21 +230,30 @@ public:
 	void nakljucno() {
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				mat[i][j] = (rand() % 199 - 99) / 100.f;
+				data[i][j] = (rand() % 199 - 99) / 100.f;
 			}
 		}
 	}
-	void funkcija(T(fun)(T)) {
+	void funkcija(T(fun)(T&)) {
 		for (size_t i = 0; i < V; i++) {
 			for (size_t j = 0; j < S; j++) {
-				mat[i][j] = fun(mat[i][j]);
+				data[i][j] = fun(data[i][j]);
 			}
 		}
 	}
-
+	Matrika<T> funkcijam(T(fun)(T&)) {
+		Matrika<T> temp(V, S);
+		for (size_t i = 0; i < V; i++) {
+			for (size_t j = 0; j < S; j++) {
+				temp(i,j) = fun(data[i][j]);
+			}
+		}
+		return temp;
+	}
+	
 	~Matrika() {}
 private:
 private:
-	vector<vector<T>> mat;
+	vector<vector<T>> data;
 	size_t S, V;
 };
